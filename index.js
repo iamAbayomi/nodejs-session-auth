f8759e39b100dc57a1b80ba1a723a1e73255f693
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-06-29 13:55:31 +0100

    Intial Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const auth = require('./auth');
+
+const cookieParser = require('cookie-parser')
+
+
+
+app.use(morgan('dev'));
+app.use(cookieParser('abcdef-12345'))
+
+app.use(session({
+    name:'session-id',
+    secret:'123456xxx',
+    saveUninitialized:false,
+    resave:false,
+    store:new FileStore()
+}))
+
+
+app.get('/', (req, res)=> {
+    res.statusCode = 200;
+    res.end("Welcome to your express app! \n Making it work  yeah r fd")
+})
+
+app.use(auth)
+app.get('/secret', (req,res) => {
+    res.status = 200;
+    res.end("*****");
+});
+
+const server = http.createServer(app);
+
+server.listen(3001, 'localhost', ()=>{
+    console.log("Server is live at localhost:3001");
+})
